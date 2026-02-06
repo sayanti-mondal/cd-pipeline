7cf8eee15b166153d63a4fefd03062b2f33c5e64
sayanti-mondal <110446789+sayanti-mondal@users.noreply.github.com>
2026-02-06 18:56:29 +0530

    Add script to install Terraform and aws cli
    This script installs Terraform and AWS CLI on a Linux system.

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+### install Terraform ####
+sudo apt-get update
+sudo apt-get install -y gnupg software-properties-common wget
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt-get update
+sudo apt-get install -y terraform
+terraform version
+
+##### install aws-cli ####
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
