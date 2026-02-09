abb9b3badd2a119dfae6069fb6f5b26900a15b60
sayanti-mondal <110446789+sayanti-mondal@users.noreply.github.com>
2026-02-09 16:50:19 +0530

    Update outputs.tf

--- terraforrm/outputs.tf
+++ terraforrm/outputs.tf
@@ -1,13 +1,14 @@
- output "public_ip"{
-    value =  aws_instance.terra_instance.public_ip
- }
+# output "public_ip"{
+ #   value =  aws_instance.terra_instance.public_ip
+ #}
 
 # output block for for_each
-#output "public_ip" {
- #   value = [
-  #      for instance in aws_instance.terra_instance : 
-   #     instance.public_ip 
-    #]
-#}
+output "public_ip" {
+    value = [
+        for instance in aws_instance.terra_instance : 
+        instance.public_ip 
+    ]
+}
+
 
 
