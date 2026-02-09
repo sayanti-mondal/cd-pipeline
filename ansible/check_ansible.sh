bc2b526033becdbd89b86576cd5778fd082954be
sayanti-mondal <110446789+sayanti-mondal@users.noreply.github.com>
2026-02-09 15:17:42 +0530

    Update check_ansible.sh

--- ansible/check_ansible.sh
+++ ansible/check_ansible.sh
@@ -8,6 +8,8 @@ if ! command -v ansible >/dev/null 2>&1; then
     sudo apt install -y software-properties-common
     sudo add-apt-repository --yes --update ppa:ansible/ansible
     sudo apt install -y ansible-core
+    ansible-galaxy collection install amazon.aws community.aws
+
 
 else
     echo "Ansible already installed"
