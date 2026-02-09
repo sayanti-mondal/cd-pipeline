b50edae0221343de9d7f96bf40907f67f7a30df7
sayanti-mondal <110446789+sayanti-mondal@users.noreply.github.com>
2026-02-09 09:44:55 +0530

    keys.tf

---
+++ terraforrm/keys.tf
@@ -0,0 +1,15 @@
+resource "tls_private_key" "ssh_key" {
+  algorithm = "ED25519"
+}
+
+resource "aws_key_pair" "terra_key" {
+  key_name   = "terra-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+
+resource "local_file" "ansible_private_key" {
+  filename        = "${path.module}/ansible_ssh_key.pem"
+  content         = tls_private_key.ssh_key.private_key_pem
+  file_permission = "0600"
+}
