f714176eefd124fe47766c92db491a8618b5b751
sayanti-mondal <110446789+sayanti-mondal@users.noreply.github.com>
2026-02-09 09:43:10 +0530

    Comment out key pair resource in ec2.tf
    Comment out the AWS key pair resource configuration.

--- terraforrm/ec2.tf
+++ terraforrm/ec2.tf
@@ -1,8 +1,8 @@
 #create key pair
-resource "aws_key_pair" "terra_key" {
-  key_name   = "terra-key"
-  public_key = file("~/.ssh/id_ed25519.pub")
-}
+#resource "aws_key_pair" "terra_key" {
+ # key_name   = "terra-key"
+  #public_key = file("~/.ssh/id_ed25519.pub")
+#}
 
 # create vpc
 resource "aws_default_vpc" "default" {
